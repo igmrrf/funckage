39586f052cd2aedeb4b32aca0d1b64cdec53fb49
thrila <ifeanyichukwundubuizu@gmail.com>
2023-06-15 21:21:58 +0100

    remove - js files

--- src/data/string.ts
+++ src/data/string.ts
@@ -1,4 +1,5 @@
 import { countryCodes } from '../constants';
+import { log } from '../simple';
 
 export const capitalize = (word: string) => {
   return word.charAt(0).toUpperCase() + word.slice(1);
@@ -16,11 +17,11 @@ export const removeCountryCode = (numbers: number[]) => {
 export const addCountryCode = (numbers: number[]) => {
   const cleaned = numbers.map((each) =>
     each.toString().substring(0, 3) === '234' ||
-    each.toString().charAt(0) === '233'
+      each.toString().charAt(0) === '233'
       ? each.toString()
       : each.toString().charAt(0) === '0'
-      ? '234' + each.toString().slice(1)
-      : '234' + each.toString()
+        ? '234' + each.toString().slice(1)
+        : '234' + each.toString()
   );
   return cleaned;
 };
